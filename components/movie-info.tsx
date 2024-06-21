8951c1f7a5172e36ef98c230bb17d228672a3360
Juno <anint3647@gmail.com>
2024-06-21 16:53:45 +0900

    Error Handling

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -2,9 +2,10 @@ import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id: string) {
   console.log(`Fetching movies: ${Date.now()}`);
-  //   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json;
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error("something broke...");
+  //   const response = await fetch(`${API_URL}/${id}`);
+  //   return response.json;
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
