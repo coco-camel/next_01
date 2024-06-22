06774be90cfe8babf8c8de9187dbc2049fa1a742
Juno <anint3647@gmail.com>
2024-06-22 21:45:51 +0900

    Movie styles

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -9,7 +9,7 @@ export default function Navigation() {
   const [count, setCount] = useState(0);
   return (
     <nav className={styles.nav}>
-      <ul className={styles.list}>
+      <ul>
         <li>
           <Link href="/">Home</Link>
           {path === "/" ? "🔥" : ""}
@@ -17,9 +17,6 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>
           {path === "/about-us" ? "🔥" : ""}
-        </li>
-        <li>
-          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
